c60d119fe517ecd789f500e19d509e4f585703b7
Michal Bobik <mikebobik1811@gmail.com>
2017-11-22 21:36:09 +0100

    Update

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+//console.log('Hello, ' + name);
+/*Both alert and console.log functions
+have been commented out using a single-line js comment.
+This one is a multiple-line js comment*/
